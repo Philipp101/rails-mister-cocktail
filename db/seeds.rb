9b2420578fee6bc8ac14ccac03e2020b3054018e
Philipp Schiessler <philipp.schiessler@mail.de>
2020-11-12 16:47:58 +0100

    created files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+puts "clearing data"
+Ingredient.delete_all
+puts "generating ingredients"
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingr_serialized = open(url).read
+ingrs = JSON.parse(ingr_serialized)
+ingrs["drinks"].each do |ingr|
+  Ingredient.create(name: ingr["strIngredient1"])
+end
+puts "done"
